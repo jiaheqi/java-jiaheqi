2e79843980632f3b5721c1447841ac3866fb47ac
贾赫琦 <jiaheqi@hualala.com>
2020-04-26 17:12:04 +0800

    httpclient练习注释添加

--- jiaheqi-httpclient/src/main/java/jiaheqi/httpclient/HttpGetNewSample.java
+++ jiaheqi-httpclient/src/main/java/jiaheqi/httpclient/HttpGetNewSample.java
@@ -12,18 +12,23 @@ public class HttpGetNewSample {
 
 
     public static void main(String[] args) {
-
+        //1、初始化httpClient对象
         CloseableHttpClient httpClient = HttpClients.createDefault();
         String url = "http://www.baidu.com";
+        //2、使用get方法请求访问获取url信息
         HttpGet httpGet = new HttpGet(url);
         CloseableHttpResponse response = null;
         InputStream inputStream = null;
 
         try {
+         //3、执行get请求，response用于接收返回
             response = httpClient.execute(httpGet);
+         //打印链接状态码，判断链接是否建立成功
             System.out.println(response.getStatusLine().getStatusCode());
+         //4、new实体接收响应返回信息
             HttpEntity httpEntity = response.getEntity();
             if (httpEntity!=null){
+          //5、字节流读取试题内容，并输出在控制台
                 inputStream = httpEntity.getContent();
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
                 String line="";
